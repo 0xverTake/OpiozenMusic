7bc67e44ff653469a49c2daf6120f48391233264
trinityIV <140190686+trinityIV@users.noreply.github.com>
2025-06-25 10:45:54 +0200

    1.7 ready

--- start-lavalink.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Lavalink server...
-cd "%~dp0lavalink"
-java -jar Lavalink.jar
-pause
--- start-lavalink.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Starting Lavalink server..."
-cd "$(dirname "$0")/lavalink"
-java -jar Lavalink.jar
